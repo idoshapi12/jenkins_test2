1da6754af6430c7f9e969632ce08eb3a114e3257
idos <idoshapi12@gmail.com>
2020-07-07 12:14:49 +0300

    change prints

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ import sys
 a = rand.randint(0, 3)
 b = rand.randint(0, 2)
 def main():
-
-	print (str(sys.argv[1]))
-	print (str(sys.argv[2]))
 	print (str(sys.argv[3]))
 	if str(sys.argv[3]) == "test_inputs":
+		print (str(sys.argv[1]))
+		print (str(sys.argv[2]))
 		if int(str(sys.argv[1])) + int(str(sys.argv[2])) <= 2:
 			sys.exit("sum is smaller than 3")
 	elif str(sys.argv[3]) == "test_rand":
-		if a + b <= 4:
+		if a + b <= 2:
+			print(a, b)
 			sys.exit("sum is smaller than 3")
 
 
